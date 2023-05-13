95b04ee915a17306ed846592fd5c67848a325be6
Dharani Kumar Gopavaram <>
2023-05-14 01:47:37 +0530

    Inclusion polymorphism

---
+++ src/main/scala/ObjectOrientedProgramming/Chapter4_InclusionPolymorphism/Shape.scala
@@ -0,0 +1,44 @@
+package ObjectOrientedProgramming.Chapter4_InclusionPolymorphism
+class Shape {
+  def area : Double = 0.0
+  def perimeter : Double = 0.0
+}
+
+/*
+  Inheritance :-
+  We can inherit classes in scala using extends keyword
+  Inheritance provides two things
+  1. Code re-usability -- Since we have access to the parent class methods
+  2. Subtyping -- We can pass child objects to the methods defined to accept parent type
+
+*/
+
+class Rectangle(val width : Double,val height : Double) extends Shape {
+  override def area: Double = width * height // we are overriding the parent class methods hence we are using override method
+  override def perimeter: Double = 2 * (width + height)
+}
+
+class Circle(val radius : Double) extends Shape {
+  override def area: Double = Math.PI * radius * radius
+  override def perimeter: Double = 2 * Math.PI * radius
+
+}
+
+class Square(val length : Double) extends Rectangle(length,length)
+object Shape {
+  def main(args : Array[String]) : Unit = {
+    val rect = new Rectangle(3,4)
+    printShapeData(rect)
+    val circle = new Circle(10)
+    printShapeData(circle)
+    val square = new Square(10)
+    printShapeData(square)
+  }
+
+  //even though we defined printShapeData which accepts Shape object as we can pass Rectangle and Circle objects since they are subtype of the parent type Shape
+  //This is the power of inclusion polymorphism
+  private def printShapeData(s : Shape) : Unit = {
+    println(s"Area :- ${s.area}")
+    println(s"Perimeter :- ${s.perimeter}")
+  }
+}
